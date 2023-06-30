0840c0d638c0fad5ac0ed93d658e16857cb8c114
Nicklas Wessel <ndw@projectum.com>
2023-06-30 11:04:36 +0200

    Advanced vs simple

--- SimpleRebus/Controllers/TestingController.cs
+++ SimpleRebus/Controllers/TestingController.cs
@@ -33,11 +33,12 @@ public class TestingController : ControllerBase
     }
 
     //Subscribe to a topic
-    [HttpPost("subscribe")]
+    [HttpPost("advanced/subscribe")]
     public async System.Threading.Tasks.Task Subscribe()
     {
         try
         {
+            //works
             await _bus.Advanced.Topics.Subscribe("project");
         }
         catch (Exception e)
@@ -47,12 +48,43 @@ public class TestingController : ControllerBase
     }
 
     //Unsubscribe from a topic 
-    [HttpPost("unsubscribe")]
+    [HttpPost("advanced/unsubscribe")]
     public async System.Threading.Tasks.Task Unsubscribe()
     {
         try
         {
+            //works
             await _bus.Advanced.Topics.Unsubscribe("project");
+        }
+        catch (Exception e)
+        {
+        }
+
+    }
+
+    //subscrive simple
+    [HttpPost("subscribe")]
+    public async System.Threading.Tasks.Task SubscribeSimple()
+    {
+        try
+        {
+            //Doesnt work
+            await _bus.Subscribe<Project>();
+        }
+        catch (Exception e)
+        {
+        }
+
+    }
+
+    //unsubscribe simple
+    [HttpPost("unsubscribe")]
+    public async System.Threading.Tasks.Task UnsubscribeSimple()
+    {
+        try
+        {
+            //Doesnt work
+            await _bus.Unsubscribe<Project>();
         }
         catch (Exception e)
         {
