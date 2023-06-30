60368a0ad3d1de39a90a60a7cb8691ddc9e3a7fd
Nicklas Wessel <ndw@projectum.com>
2023-06-30 14:54:11 +0200

    X

--- SimpleRebus/Controllers/TestingController.cs
+++ SimpleRebus/Controllers/TestingController.cs
@@ -13,100 +13,35 @@ public class TestingController : ControllerBase
     {
         _bus = bus;
     }
-
+    //Publish a message
     [HttpPost("advanced/publish")]
     public async Task Publish([FromBody] Project project)
-    {
-        try
-        {
-
-            //works
-            await _bus.Advanced.Topics.Publish("project", project);
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-        
-    }
+        => await _bus.Advanced.Topics.Publish("project", project);
 
     //Subscribe to a topic
     [HttpPost("advanced/subscribe")]
     public async Task Subscribe()
-    {
-        try
-        {
-            //works
-            await _bus.Advanced.Topics.Subscribe("project");
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-
-    }
+        => await _bus.Advanced.Topics.Subscribe("project");
 
     //Unsubscribe from a topic 
     [HttpPost("advanced/unsubscribe")]
-    public async Task Unsubscribe()
-    {
-        try
-        {
-            //works
-            await _bus.Advanced.Topics.Unsubscribe("project");
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
+    public async Task Unsubscribe() =>
+        await _bus.Advanced.Topics.Unsubscribe("project");
 
-    }
+    // BELOW DOES NOT WORK - Trying to figure out why
 
     //publish simple
     [HttpPost("publish")]
     public async Task PublishSimple([FromBody] MyEvent myEvent)
-    {
-        try
-        {
-            //Doesnt work
-            await _bus.Publish(myEvent);
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-
-    }
+        => await _bus.Publish(myEvent);
 
     //subscrive simple
     [HttpPost("subscribe")]
-    public async Task SubscribeSimple()
-    {
-        try
-        {
-            //Doesnt work
-            await _bus.Subscribe<MyEvent>();
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-
-    }
+    public async Task SubscribeSimple() 
+        => await _bus.Subscribe<MyEvent>();
 
     //unsubscribe simple
     [HttpPost("unsubscribe")]
     public async Task UnsubscribeSimple()
-    {
-        try
-        {
-            //Doesnt work
-            await _bus.Unsubscribe<MyEvent>();
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
-
-    }
+        => await _bus.Unsubscribe<MyEvent>();
 }
